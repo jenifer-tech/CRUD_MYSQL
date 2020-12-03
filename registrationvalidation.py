2836e86cb9e0a7b7a0a2e1541875dc71bbed0410
Your Name <you@example.com>
2020-12-03 16:10:26 +0530

    jeni

--- registrationvalidation.py
+++ registrationvalidation.py
@@ -30,90 +30,93 @@ def exe_quer(conn,cursor):
     return conn,cursor
 
     
-def validation(new_fname,new_lname):
+def validation(new_fname,new_lname,new_password,new_email,new_mobileno):
+    if not new_fname or not new_lname or not new_password or not new_email or not new_mobileno :
+        return "Please enter all values"
     if not re.match  (r'[A-Za-z]+',new_fname):
-        return "only string first name",400
-    if not re.match  (r'[A-Za-z]+',new_lname):
-        return "only string last name",400
+        return "First name only string."
+    elif not re.match  (r'[A-Za-z]+',new_lname):
+        return "Last name only string"
+    elif not re.match  (r'[A-Za-z0-9]+',new_password):
+        return "Passwod name only string"
+    elif not re.match  (r'[^@]+@[^@]+\.[^@]+',new_email):   
+        return "Invalid Email address " 
+    elif not re.search(re.compile(r'(\+91)?(-)?\s*?(91)?\s*?(\d{3})-?\s*?(\d{3})-?\s*?(\d{4})'),new_mobileno):    
+        return"Mobile no must contain only numbers"        
 
-@app.route("/signup",methods=["POST"])
-def signup():
-    a,b=exe_quer('conn','cursor')
+    return ''
+
+def select(new_email,new_password):
+   
+    a,b=exe_quer('conn','cursor') 
+    se="""SELECT * FROM login WHERE email=%s AND password=%s"""
+    b.execute(se,(new_email,new_password))
+    account=b.fetchone()
+    a.commit()
+    return account
     
 
+    
+@app.route("/signup",methods=['POST'])
+def signup():
+    if request.method!="POST" :
+        return jsonify({"message":"only post method is  allowed"}),400
+              
    
+    
     new_fname=request.form['fname']
     new_lname=request.form['lname']
-    new_password=request.form['password']  
+    new_password=request.form['password'] 
     new_email  =request.form['email'] 
     new_mobileno=request.form['mobileno']
-    c,d=validation('new_fname','new_lname')
-    if new_fname and new_lname and new_password and new_email and new_mobileno and  request.method=="POST" :
-        
-        b.execute('SELECT * FROM login WHERE fname=%s' ,(new_fname,))
-            
-        acc=b.fetchone()
-        if acc:
-            return "Account already exist!"            
-        elif not re.match  (r'[^@]+@[^@]+\.[^@]+',new_email):   
-            return "Invalid Email address " ,400 
-        elif new_fname:    
-            return c
-        elif new_lname:    
-            return d
-        
-        elif not re.search(re.compile(r'(\+91)?(-)?\s*?(91)?\s*?(\d{3})-?\s*?(\d{3})-?\s*?(\d{4})'),new_mobileno):    
-            return "Mobile no must contain only numbers",400    
-        
-        else:
-            sq="""INSERT INTO login (fname,lname,password,email,mobileno) 
-                            VALUES (%s,%s,%s,%s,%s)"""
-                                
-            b.execute(sq,(new_fname,new_lname,new_password,new_email,new_mobileno))
-            a.commit()
-            return "You have successfully registered" ,201            
-    elif not new_fname  or not new_lname or not new_email or not new_mobileno or not new_password:
-        return "pls enter all the details" ,400        
+    
+
+    error_msg=validation(new_fname,new_lname,new_password,new_email,new_mobileno)
+    if error_msg:
+        return jsonify({"message":error_msg}),400
+       
+    a,b=exe_quer('conn','cursor')    
+    b.execute('SELECT * FROM login WHERE fname=%s' ,(new_fname,))      
+    acc=b.fetchone()
+    if acc:
+        return jsonify({"message":"Account already exist!"}),200             
+    else:
+        sq="""INSERT INTO login (fname,lname,password,email,mobileno) 
+                        VALUES (%s,%s,%s,%s,%s)"""
                             
-                         
-
-
-              
+        b.execute(sq,(new_fname,new_lname,new_password,new_email,new_mobileno))
+        a.commit()
+        return jsonify({"message":"You have successfully registered"}) ,201            
+        
         
 @app.route('/signin',methods=["POST","DELETE"])
 def signin():
         a,b=exe_quer('conn','cursor')
+        new_email  =request.form['email'] 
+        new_password=request.form['password'] 
 
-        
-        
+        val=select(new_email,new_password)
+
         if request.method == 'POST':
-            new_email  =request.form['email'] 
-            new_password=request.form['password']  
-        
-            se="""SELECT * FROM login WHERE email=%s AND password=%s"""
-            b.execute(se,(new_email,new_password))
-            account=b.fetchone()
-            a.commit()
-            
-            if account:
-                
-                new_email=account['new_email']
-                new_password=account['new_password']
-                return 'Logged in successfully!',202
-                
-                
+                      
+            if val:
+                return jsonify({"message":"Logged in successfully!"}),202            
+              
             else:
-                return "Enter correct email or password ",401
+                return jsonify({"message":"Enter correct email or password"}),401
 
         if request.method=='DELETE': 
-            new_email  =request.form['email'] 
-            new_password=request.form['password'] 
-            sql_del="""DELETE FROM login WHERE email=%s AND password=%s """ 
-            b=b.execute(sql_del,(new_email,new_password,))
-            a.commit()
-            return "The email id: {} and password : {}   has been deleted  successfully!. ".format(new_email,new_password),200
-        return "Please enter correct email or password ",401     
+            
+            if val:
+                sql_del="""DELETE FROM login WHERE email=%s AND password=%s """ 
+                b.execute(sql_del,(new_email,new_password,))
+                a.commit()
+            
+                return jsonify({"message":"You account  has been deleted  successfully!."}),200
+            else:    
+                return jsonify({"message":"Please enter correct email or password "}),401     
 
 if __name__=='__main__':
     app.run(debug=True)
 
+          
